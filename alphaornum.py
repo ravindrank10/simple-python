6bbc79317f68ea61b868a5bf0c25f0730591b742
ravindrank10 <47316084+ravindrank10@users.noreply.github.com>
2019-02-04 08:45:33 +0000

    Add files via upload

---
+++ alphaornum.py
@@ -0,0 +1,7 @@
+s=input()
+if s.isalpha():
+    print ("Alphabet")
+elif s.isnumeric():
+    print ("Number")
+else:
+ print ("spl chr")
---
+++ vowel_r_not.py
@@ -0,0 +1,6 @@
+k=input()
+if k=='a' or k=='e' or k=='i' or k=='o' or k=='u':
+    print("Vowel")
+else:
+    print("Consonant")
+
